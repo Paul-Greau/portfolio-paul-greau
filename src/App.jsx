97e76c515cbfcf69afb0098c4d4ba57c893e0d92
Paul-Greau <mrgreau@gmail.com>
2023-04-20 13:12:06 +0200

    initial commit

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div>
+      <p>
+        Hello les amis
+      </p>
+    </div>
+  )
+}
+
+export default App
