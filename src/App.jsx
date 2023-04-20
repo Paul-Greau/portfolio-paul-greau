5631361ba5011c69b9e213a20eec71dc4935afc4
Paul-Greau <mrgreau@gmail.com>
2023-04-20 15:02:31 +0200

    add eslint rules

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,9 @@
-import { useState } from 'react'
-
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <div>
-      <p>
-        Hello les amis
-      </p>
+      <p>Hello world</p>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
